5ad94cb5d267e8d066409644ab2a2bb40e2f7e4d
Nishant Kumar Sinha <nishant.sinha@Nishant-Kumar-Sinha.local>
2014-12-04 17:20:43 +0530

    Adding text as pw and username

--- timeline/php/db_connection.php
+++ timeline/php/db_connection.php
@@ -1,5 +1,5 @@
 <?php
-	$db_conx=mysqli_connect("localhost","hritupon","suman","timeline");
+	$db_conx=mysqli_connect("localhost","username","password","timeline");
 	//Evaluate the connection
 	if(mysqli_connect_errno()){
 		echo mysqli_connect_error();
